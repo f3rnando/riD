1db8c0a002d01bdeb365d3376a53d0d9a5144a45
Fernando <correo@fernando.com.ar>
2015-06-04 04:06:49 -0300

    dirty socketize

--- js/id/core/difference.js
+++ js/id/core/difference.js
@@ -7,170 +7,182 @@
     child and parent relationships.
  */
 iD.Difference = function(base, head) {
-    var changes = {}, length = 0;
+  var changes = {},
+    length = 0;
 
-    function changed(h, b) {
-        return h !== b && !_.isEqual(_.omit(h, 'v'), _.omit(b, 'v'));
+  function changed(h, b) {
+    return h !== b && !_.isEqual(_.omit(h, 'v'), _.omit(b, 'v'));
+  }
+
+  _.each(head.entities, function(h, id) {
+    var b = base.entities[id];
+    if (changed(h, b)) {
+      changes[id] = {
+        base: b,
+        head: h
+      };
+      length++;
+    }
+  });
+
+  _.each(base.entities, function(b, id) {
+    var h = head.entities[id];
+    if (!changes[id] && changed(h, b)) {
+      changes[id] = {
+        base: b,
+        head: h
+      };
+      length++;
+    }
+  });
+
+  function addParents(parents, result) {
+    for (var i = 0; i < parents.length; i++) {
+      var parent = parents[i];
+
+      if (parent.id in result)
+        continue;
+
+      result[parent.id] = parent;
+      addParents(head.parentRelations(parent), result);
+    }
+  }
+
+  var difference = {};
+
+  difference.length = function() {
+    return length;
+  };
+
+  difference.changes = function() {
+    return changes;
+  };
+
+  difference.extantIDs = function() {
+    var result = [];
+    _.each(changes, function(change, id) {
+      if (change.head) result.push(id);
+    });
+    return result;
+  };
+
+  difference.modified = function() {
+    var result = [];
+    _.each(changes, function(change) {
+      if (change.base && change.head) result.push(change.head);
+    });
+    return result;
+  };
+
+  difference.created = function() {
+    var result = [];
+    _.each(changes, function(change) {
+      if (!change.base && change.head) result.push(change.head);
+    });
+    return result;
+  };
+
+  difference.deleted = function() {
+    var result = [];
+    _.each(changes, function(change) {
+      if (change.base && !change.head) result.push(change.base);
+    });
+    return result;
+  };
+
+  difference.summary = function() {
+    var relevant = {};
+
+    function addEntity(entity, graph, changeType) {
+      relevant[entity.id] = {
+        entity: entity,
+        graph: graph,
+        changeType: changeType
+      };
     }
 
-    _.each(head.entities, function(h, id) {
-        var b = base.entities[id];
-        if (changed(h, b)) {
-            changes[id] = {base: b, head: h};
-            length++;
-        }
-    });
-
-    _.each(base.entities, function(b, id) {
-        var h = head.entities[id];
-        if (!changes[id] && changed(h, b)) {
-            changes[id] = {base: b, head: h};
-            length++;
-        }
-    });
-
-    function addParents(parents, result) {
-        for (var i = 0; i < parents.length; i++) {
-            var parent = parents[i];
-
-            if (parent.id in result)
-                continue;
-
-            result[parent.id] = parent;
-            addParents(head.parentRelations(parent), result);
-        }
+    function addParents(entity) {
+      var parents = head.parentWays(entity);
+      for (var j = parents.length - 1; j >= 0; j--) {
+        var parent = parents[j];
+        if (!(parent.id in relevant)) addEntity(parent, head, 'modified');
+      }
     }
 
-    var difference = {};
+    _.each(changes, function(change) {
+      if (change.head && change.head.geometry(head) !== 'vertex') {
+        addEntity(change.head, head, change.base ? 'modified' : 'created');
 
-    difference.length = function() {
-        return length;
-    };
+      } else if (change.base && change.base.geometry(base) !== 'vertex') {
+        addEntity(change.base, base, 'deleted');
 
-    difference.changes = function() {
-        return changes;
-    };
+      } else if (change.base && change.head) { // modified vertex
+        var moved = !_.isEqual(change.base.loc, change.head.loc),
+          retagged = !_.isEqual(change.base.tags, change.head.tags);
 
-    difference.extantIDs = function() {
-        var result = [];
-        _.each(changes, function(change, id) {
-            if (change.head) result.push(id);
-        });
-        return result;
-    };
-
-    difference.modified = function() {
-        var result = [];
-        _.each(changes, function(change) {
-            if (change.base && change.head) result.push(change.head);
-        });
-        return result;
-    };
-
-    difference.created = function() {
-        var result = [];
-        _.each(changes, function(change) {
-            if (!change.base && change.head) result.push(change.head);
-        });
-        return result;
-    };
-
-    difference.deleted = function() {
-        var result = [];
-        _.each(changes, function(change) {
-            if (change.base && !change.head) result.push(change.base);
-        });
-        return result;
-    };
-
-    difference.summary = function() {
-        var relevant = {};
-
-        function addEntity(entity, graph, changeType) {
-            relevant[entity.id] = {
-                entity: entity,
-                graph: graph,
-                changeType: changeType
-            };
+        if (moved) {
+          addParents(change.head);
         }
 
-        function addParents(entity) {
-            var parents = head.parentWays(entity);
-            for (var j = parents.length - 1; j >= 0; j--) {
-                var parent = parents[j];
-                if (!(parent.id in relevant)) addEntity(parent, head, 'modified');
-            }
+        if (retagged || (moved && change.head.hasInterestingTags())) {
+          addEntity(change.head, head, 'modified');
         }
 
-        _.each(changes, function(change) {
-            if (change.head && change.head.geometry(head) !== 'vertex') {
-                addEntity(change.head, head, change.base ? 'modified' : 'created');
+      } else if (change.head && change.head.hasInterestingTags()) { // created vertex
+        addEntity(change.head, head, 'created');
 
-            } else if (change.base && change.base.geometry(base) !== 'vertex') {
-                addEntity(change.base, base, 'deleted');
+      } else if (change.base && change.base.hasInterestingTags()) { // deleted vertex
+        addEntity(change.base, base, 'deleted');
+      }
+    });
 
-            } else if (change.base && change.head) { // modified vertex
-                var moved    = !_.isEqual(change.base.loc,  change.head.loc),
-                    retagged = !_.isEqual(change.base.tags, change.head.tags);
+    return d3.values(relevant);
+  };
 
-                if (moved) {
-                    addParents(change.head);
-                }
+  difference.complete = function(extent) {
+    var result = {},
+      id, change;
 
-                if (retagged || (moved && change.head.hasInterestingTags())) {
-                    addEntity(change.head, head, 'modified');
-                }
+    for (id in changes) {
+      change = changes[id];
 
-            } else if (change.head && change.head.hasInterestingTags()) { // created vertex
-                addEntity(change.head, head, 'created');
+      var h = change.head,
+        b = change.base,
+        entity = h || b;
 
-            } else if (change.base && change.base.hasInterestingTags()) { // deleted vertex
-                addEntity(change.base, base, 'deleted');
-            }
-        });
+      if (extent &&
+        (!h || !h.intersects(extent, head)) &&
+        (!b || !b.intersects(extent, base)))
+        continue;
 
-        return d3.values(relevant);
-    };
+      result[id] = h;
 
-    difference.complete = function(extent) {
-        var result = {}, id, change;
+      if (entity.type === 'way') {
+        var nh = h ? h.nodes : [],
+          nb = b ? b.nodes : [],
+          diff, i;
 
-        for (id in changes) {
-            change = changes[id];
-
-            var h = change.head,
-                b = change.base,
-                entity = h || b;
-
-            if (extent &&
-                (!h || !h.intersects(extent, head)) &&
-                (!b || !b.intersects(extent, base)))
-                continue;
-
-            result[id] = h;
-
-            if (entity.type === 'way') {
-                var nh = h ? h.nodes : [],
-                    nb = b ? b.nodes : [],
-                    diff, i;
-
-                diff = _.difference(nh, nb);
-                for (i = 0; i < diff.length; i++) {
-                    result[diff[i]] = head.hasEntity(diff[i]);
-                }
-
-                diff = _.difference(nb, nh);
-                for (i = 0; i < diff.length; i++) {
-                    result[diff[i]] = head.hasEntity(diff[i]);
-                }
-            }
-
-            addParents(head.parentWays(entity), result);
-            addParents(head.parentRelations(entity), result);
+        diff = _.difference(nh, nb);
+        for (i = 0; i < diff.length; i++) {
+          result[diff[i]] = head.hasEntity(diff[i]);
         }
 
-        return result;
-    };
+        diff = _.difference(nb, nh);
+        for (i = 0; i < diff.length; i++) {
+          result[diff[i]] = head.hasEntity(diff[i]);
+        }
+      }
 
-    return difference;
+      addParents(head.parentWays(entity), result);
+      addParents(head.parentRelations(entity), result);
+    }
+    console.log(extent);
+    socket.emit('diff-result', {
+      'diff': result,
+      'from': ''
+    });
+    return result;
+  };
+
+  return difference;
 };
