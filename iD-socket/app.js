fa88a8e0e23ab27d833d23deec44d1eb8821d0f4
Fernando <correo@fernando.com.ar>
2015-06-04 04:16:07 -0300

    Revert "tiny app that listens 3008 (fix)"
    This reverts commit 95480aab83b08a796afa172daa58f716e447ffe8.

--- iD-socket/app.js
+++
@@ -1,6 +0,0 @@
-var http = require("http");
-var app = require("express")();
-var server = http.createServer(app);
-var sockets = require("./lib/sockets")(app, server);
-
-server.listen(process.env.PORT || 3008);
