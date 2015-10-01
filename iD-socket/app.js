2b591e1a868afd05e2887c16af50b94fc64cbc73
Fernando <correo@fernando.com.ar>
2015-10-01 18:43:23 -0300

    refactored socket server

--- iD-socket/app.js
+++ iD-socket/app.js
@@ -1,6 +1,23 @@
-var http = require("http");
-var app = require("express")();
-var server = http.createServer(app);
-var sockets = require("./lib/sockets")(app, server);
+var server = require('http').createServer(),
+    io     = require('socket.io')(server);
 
-server.listen(process.env.PORT || 3008);
+
+io.on('connection', function(socket){
+
+  var room = socket.handshake['query']['r_var'];
+
+  socket.join(room);
+  console.log('user joined georoom #'+room);
+
+  socket.on('disconnect', function() {
+    socket.leave(room)
+    console.log('user disconnected');
+  });
+
+  socket.on('difference-out', function(data){
+    io.to(room).emit('difference-in', data);
+  });
+
+});
+
+server.listen(3008);
